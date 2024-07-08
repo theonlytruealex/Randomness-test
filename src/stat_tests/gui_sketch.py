5045b4828a802a1a5d00df614149ae68e5d40255
andrei811 <adi@leibniz>
2024-07-08 16:32:19 +0300

    modified the gui

--- src/stat_tests/gui_sketch.py
+++ src/stat_tests/gui_sketch.py
@@ -25,16 +25,12 @@ class App(customtkinter.CTk):
         # configure grid layout (4x4)
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
-        self.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9), weight=1)
-        
-        # set default font to be used for the app...
-        self.DEFAULT_FONT = customtkinter.CTkFont(family="Calibri", size=15)
+        self.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), weight=1)
 
         # create sidebar frame with widgets
-        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
-        self.sidebar_frame.grid(row=0, column=0, rowspan=10, sticky="nsew")
-        # the next line brings about that annoying space in the menu 
-        self.sidebar_frame.grid_rowconfigure(5, weight=1)
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=10)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=11, sticky="nsew")
+        
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Interactive Randomness Test", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
         self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.monobit_event, text = "Monobit Test")
@@ -43,31 +39,40 @@ class App(customtkinter.CTk):
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
         self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.autocorr_event, text = "Autocorrelation Test")
         self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
-        self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, command=self.serial_event, text = "Serial Test")
+        self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, command=self.runs_event, text = "Runs Test")
         self.sidebar_button_4.grid(row=4, column=0, padx=20, pady=10)
+        self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, command=self.serial_event, text = "Serial Test")
+        self.sidebar_button_4.grid(row=5, column=0, padx=20, pady=10)
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
-        self.appearance_mode_label.grid(row=6, column=0, padx=20, pady=(10, 0))
+        # the next line brings about that annoying space in the menu 
+        self.sidebar_frame.grid_rowconfigure(6, weight=1)
+        self.appearance_mode_label.grid(row=7, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
                                                                        command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=7, column=0, padx=20, pady=(10, 10))
+        self.appearance_mode_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 10))
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
-        self.scaling_label.grid(row=8, column=0, padx=20, pady=(10, 0))
+        self.scaling_label.grid(row=9, column=0, padx=20, pady=(10, 0))
         self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=9, column=0, padx=20, pady=(10, 20))
+        self.scaling_optionemenu.grid(row=10, column=0, padx=20, pady=(10, 20))
 
         # should be created a new frame for the right hand of the gui 
         # to be implemented...
         
+        self.main_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.main_frame.grid(row=0, column=1, rowspan=10, columnspan=3, sticky="nsew")
+        self.main_frame.grid_columnconfigure((0, 1, 2), weight=1)
+        self.main_frame.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11), weight=1)
+        
         # create textbox
-        self.main_label = customtkinter.CTkLabel(self,
-        text_color="black",
-        font=self.DEFAULT_FONT,
+        self.main_label = customtkinter.CTkLabel(self.main_frame,
+        font=customtkinter.CTkFont(size=17, weight="normal"),
         text = "Select the desired Randomness Test")
-        self.main_label.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.main_label.grid(row=6, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
 
         # set default values
         self.appearance_mode_optionemenu.set("Dark")
+        self.change_appearance_mode_event("Dark")
         self.scaling_optionemenu.set("100%")
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
@@ -85,6 +90,8 @@ class App(customtkinter.CTk):
     def autocorr_event(self):
         pass
     def serial_event(self):
+        pass
+    def runs_event(self):
         pass
 if __name__ == "__main__":
     app = App()
