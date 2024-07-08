a074ec3a1647ba9ca028e7801867193989e43d6f
ancastef15 <ancastef15@yahoo.ro>
2024-07-08 12:26:59 +0300

    Designed a GUI for the main menu in guy_sketch.py

---
+++ src/stat_tests/gui_sketch.py
@@ -0,0 +1,84 @@
+# a file to help with the design of the gui
+# use it and adapt it to your own taste
+# serves as an implementation for the main menu module
+
+import tkinter
+import tkinter.messagebox
+import customtkinter
+import monobit
+import mbit
+import autocorrelation
+import serial
+
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # configure window
+        self.title("gui_sketch.py")
+        self.geometry(f"{800}x{580}")
+
+        # configure grid layout (4x4)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2, 3), weight=1)
+
+        # create sidebar frame with widgets
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=5, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Interactive Randomness Test", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.monobit_event, text = "Monobit Test")
+        self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.mbit_event, text = "M-bit Test")
+        self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.autocorr_event, text = "Autocorrelation Test")
+        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+        self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, command=self.serial_event, text = "Serial Test")
+        self.sidebar_button_4.grid(row=4, column=0, padx=20, pady=10)
+        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
+        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
+                                                                       command=self.change_appearance_mode_event)
+        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
+        self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
+                                                               command=self.change_scaling_event)
+        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
+
+        # create textbox
+        self.main_label = customtkinter.CTkLabel(self,
+        text_color="black",
+        font=("Times New Roman", 20),
+        text = "Select the desired Randomness Test")
+        self.main_label.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+
+        # set default values
+        self.appearance_mode_optionemenu.set("Dark")
+        self.scaling_optionemenu.set("100%")
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def change_scaling_event(self, new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
+
+    def monobit_event(self):
+        self.main_label.destroy()
+        monobit.monobit(self)
+    def mbit_event(self):
+        pass
+    def autocorr_event(self):
+        pass
+    def serial_event(self):
+        pass
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
--- src/stat_tests/monobit.py
+++ src/stat_tests/monobit.py
@@ -1,57 +1,41 @@
-import customtkinter as ctk
-import tkinter as tk
+import gui_sketch
+import tkinter
+import tkinter.messagebox
+import customtkinter
  
-# Basic parameters and initializations
-# Supported modes : Light, Dark, System
-ctk.set_appearance_mode("System") 
- 
-# Supported themes : green, dark-blue, blue
-ctk.set_default_color_theme("green")    
- 
-appWidth, appHeight = 400, 400
- 
-# App Class
-class App(ctk.CTk):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
- 
-        self.title("GUI Application")
-        self.geometry(f"{appWidth}x{appHeight}")
- 
-        # Length Label
-        self.lenLabel = ctk.CTkLabel(self,
-                                     text="Sequence Length")
-        self.lenLabel.grid(row=0, column=0,
-                           padx=20, pady=20,
-                           sticky="ew")
- 
-        # Length Field
-        self.lenEntry = ctk.CTkEntry(self,
-                            placeholder_text="30")
-        self.lenEntry.grid(row=0, column=1,
-                           columnspan=3, padx=20,
-                           pady=20, sticky="ew")
-        
-        # Bit Sequence Label
-        self.bitSeqLabel = ctk.CTkLabel(self,
-                                      text="Bit Sequence")
-        self.bitSeqLabel.grid(row=1, column=0,
-                            padx=20, pady=20,
-                            sticky="ew")
- 
-        # Name Entry Field
-        self.bitSeqEntry = ctk.CTkEntry(self)
-        self.bitSeqEntry.grid(row=1, column=1,
-                            columnspan=3, padx=20,
-                            pady=20, sticky="ew")
- 
-        # Generate Button
-        self.generateResultsButton = ctk.CTkButton(self,
-                                         text="Generate Results")
-        self.generateResultsButton.grid(row=5, column=1,
-                                        columnspan=2,
-                                        padx=20, pady=20,
-                                        sticky="ew")
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+def monobit(self):
+
+    # Length Label
+    self.lenLabel = customtkinter.CTkLabel(self,
+                                    text="Sequence Length")
+    self.lenLabel.grid(row=0, column=1,
+                        padx=20, pady=20,
+                        sticky="ew")
+
+    # Length Field
+    self.lenEntry = customtkinter.CTkEntry(self,
+                        width=200)
+    self.lenEntry.grid(row=0, column=2,
+                        columnspan=3, padx=20,
+                        pady=20, sticky="ew")
+    
+    # Bit Sequence Label
+    self.bitSeqLabel = customtkinter.CTkLabel(self,
+                                    text="Bit Sequence")
+    self.bitSeqLabel.grid(row=1, column=1,
+                        padx=20, pady=20,
+                        sticky="ew")
+
+    # Name Entry Field
+    self.bitSeqEntry = customtkinter.CTkEntry(self)
+    self.bitSeqEntry.grid(row=1, column=2,
+                        columnspan=3, padx=20,
+                        pady=20, sticky="ew")
+
+    # Generate Button
+    self.generateResultsButton = customtkinter.CTkButton(self,
+                                        text="Generate Results")
+    self.generateResultsButton.grid(row=4, column=1,
+                                    columnspan=2,
+                                    padx=20, pady=20,
+                                    sticky="ew")
